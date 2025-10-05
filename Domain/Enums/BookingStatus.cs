a8015cd02d864d9f54c610a0d2b79f3ee5105695
AVJayasundara <avimod736@gmail.com>
2025-10-05 12:12:40 +0530

    Add Domain Changes

--- Domain/Enums/BookingStatus.cs
+++ Domain/Enums/BookingStatus.cs
@@ -2,10 +2,10 @@
 {
     public enum BookingStatus
     {
-        Pending = 1,
-        Approved = 2,
-        InProgress = 3,
-        Completed = 4,
-        Cancelled = 5
+        Pending,      // Waiting for station operator approval
+        Confirmed,    // Approved and ready for charging
+        Cancelled,    // Cancelled by user or operator
+        Completed,    // Charging session completed
+        NoShow
     }
 }
